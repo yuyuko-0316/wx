d87e14f12cf823a5522b9122091cde13ec9c3679
yuyuko <yuyuko0316@gmail.com>
2025-05-10 13:45:41 +0800

    aaa

--- wxcloudrun/views.py
+++ wxcloudrun/views.py
@@ -23,9 +23,22 @@ def count():
     # 获取请求体参数
     params = request.get_json()
 
+    if not params:
+       return make_err_response('No JSON data provided')
+
+    try:
+        # 将数据转发到另一个服务器
+        response = requests.post('http://113.200.194.122:8988/post', json=params)
+        
+        # 返回转发后的响应
+        return make_succ_response(response.json())
+    except requests.exceptions.RequestException as e:
+         return make_err_response('缺少action参数11' + str(params))
+
+
     # 检查action参数
     if 'action' not in params:
-        return make_err_response('缺少action参数' + str(params))
+        return make_err_response('缺少action参数22' + str(params))
 
     # 按照不同的action的值，进行不同的操作
     action = params['action']
